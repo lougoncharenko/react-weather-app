e946daab29ee9c84f61275f372d97b409a490856
Louisa Goncharenko <louisa.goncharenko@shopify.com>
2022-11-28 08:50:32 -0500

    added weather results

--- src/components/WeatherInfo.tsx
+++ src/components/WeatherInfo.tsx
@@ -4,25 +4,35 @@ import './WeatherInfo.css'
 // interface Props {
 //     weatherData: 
 // }
+    // const place = data.name;
+    // const temperature = data.main.temp;
+    // const feels = data.main.feels_like;
+    // const desc = data.weather[0].description;
+    // const humid = data.main.humidity
+    // const maxTemp = data.main.temp_max;
 
 export const WeatherInfo = (props:any) => {
     const { data } = props;
+    const iconurl =
+    "http://openweathermap.org/img/wn/" +
+    `${data.cod != 404 ? data.weather[0].icon : null}` +
+    ".png";
   return (
     <section className="weatherInfo">
         <section className="cardTop">
-            <span className="weatherCondition"></span>
-            <h1 className="city"></h1>
+            <span className="weatherCondition">  {data.weather[0].description} </span>
+            <h1 className="city"> {data.name} </h1>
         </section>
         <div className='bottom-cards'>
             <section className="cardLeft">
-                <span className="temp"></span>
-                {/* insert weather icon here */}
+                <span className="temp"> Temperature: {data.main.temp} </span>
+                <img className="weather-icon" src={iconurl} alt=""  />
             </section>
             <div className ="border"> </div>
             <section className="cardRight">
-                <span className="feelsLike"></span>
-                <span className="tempMax"></span>
-                <span className="humidty"></span>
+                <span className="feelsLike"> Feels Like: {data.main.feels_like} </span>
+                <span className="tempMax"> Pressure: {data.main.pressure} </span>
+                <span className="humidty"> Humidity: {data.main.humidity} % </span>
             </section>
         </div>
 </section>
